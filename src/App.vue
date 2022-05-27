951c6de26cc60139fe45bf5b141d6c3bd826779b
owentr1369 <tamvaa1306@gmail.com>
2022-05-27 11:07:04 +0700

    lifecycle in Vuejs

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,58 @@
 <template>
   <div id="app">
-    <h1>Ep5 part 11</h1>
+    <h1>{{ title }}</h1>
+    <HelloVn />
+    <button @click="title = 'Changed with event click'">Update</button>
+    <button @click="destroyEvent">Destroy</button>
   </div>
 </template>
 
 <script>
 // v-if se remove phan tu di, con v-show se an phan tu di
-
+import HelloVn from "./components/HelloVn.vue";
 export default {
   name: "App",
+  components: { HelloVn },
   data() {
-    return {
-    };
+    return { title: "VueJS instance lifecycle" };
   },
-  methods: {},
+  methods: {
+    destroyEvent: function () {
+      this.$destroy();
+    },
+  },
+  beforeCreate: function () {
+    //
+    console.log("beforeCreate!");
+  },
+  created: function () {
+    //
+    console.log("created!");
+  },
+  beforeMount: function () {
+    //
+    console.log("beforeMount!");
+  },
+  mounted: function () {
+    //
+    console.log("mounted!");
+  },
+  beforeUpdate: function () {
+    //
+    console.log("beforeUpdate!");
+  },
+  updated: function () {
+    //
+    console.log("updated!");
+  },
+  beforeDestroy: function () {
+    //
+    console.log("beforeDestroy!");
+  },
+  destroyed: function () {
+    //
+    console.log("destroyed!");
+  },
 };
 </script>
 
