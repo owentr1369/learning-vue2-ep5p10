87770af76987cfc18d462604cd2943b48038af1a
owentr1369 <tamvaa1306@gmail.com>
2022-05-27 10:57:27 +0700

    setup

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <h1>Ep5 part 11</h1>
+  </div>
+</template>
+
+<script>
+// v-if se remove phan tu di, con v-show se an phan tu di
+
+export default {
+  name: "App",
+  data() {
+    return {
+    };
+  },
+  methods: {},
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
